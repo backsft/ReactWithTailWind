90f37fc5bf2916dc1226bb1b97140f8b06ecd829
backsft <kabirmia526@gmail.com>
2024-11-28 02:09:18 +0600

    event handaling with parameter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import './App.css';
 import Header from './Header';
 import weblogo from "./Images/preview.jpg"
 function App() {
+
+
+  let displayData = () => {
+
+    alert("Welcome to My Website")
+  }
+
+  let addData = (a,b) => {
+
+   // console.log(a+b)
+   alert(a+b)
+  }
+
   return (
 
 
     <div className='App'>
-<Header/>
-<img width={100} src={weblogo} />
-    <h1 className='text-[60px] text-black-900 font-bold'>Hello</h1>
-  
+      <Header />
+
+      <button className='bg-[yellow] p[10px] mr-4' onClick={()=>addData(20,15)}>ParameterizeEvent</button>
+      <button className='bg-[red] p[10px]' onClick={displayData}>Save</button>
+      <img width={100} src={weblogo} />
+      <h1 className='text-[60px] text-black-900 font-bold'>Hello</h1>
+
 
     </div>
 
-);
- 
+  );
+
 }
 
 export default App;
